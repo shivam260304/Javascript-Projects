0ccd1a04295825441d3ba394f7082456773aea57
shivam260304 <shivam260304@gmail.com>
2023-09-19 13:32:15 +0530

    pr1

---
+++ Social Media(CRUD)/crud.js
@@ -0,0 +1,58 @@
+let post = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+// submit (EVENTLISTENER)
+post.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    console.log("Submitted");
+    formValidation();
+})
+
+// VALIDATING FORM (success or failure)
+let formValidation =()=>{
+    if (input.value === "") {
+        msg.innerHTML = "Message is not submitted";
+        console.log("Failure");
+    }
+    else{
+        msg.innerHTML = "";
+        console.log("Success");
+        accepData();
+    }
+};
+
+// DATA ACCEPTED IN THE data OBJECT
+let data ={};
+let accepData = () =>{
+    data["text"] = input.value;
+    console.log(data);
+    createPost();
+}
+
+// Displaying Data on the Screen (Very IMPORTANT)
+let createPost =()=>{
+    posts.innerHTML += 
+    `<div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onclick="up(this)" class="fas fa-edit"></i>
+      <i onclick="d(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>`;
+    input.value="";
+}
+
+// delete (inline event Listener)
+let d =(e) =>{
+    e.parentElement.parentElement.remove();
+}
+
+let up = (e)=>{
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+}
+
+
+
